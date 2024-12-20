497689e7f2c6ab365aacea4d1f03e7e66855e487
Daniel Hurtado <81983296+DHurtado714-itesm@users.noreply.github.com>
2024-12-20 22:59:02 +0100

    feat(Swap): add modal for token selection and update token handling logic

--- dex/src/components/Swap.js
+++ dex/src/components/Swap.js
@@ -13,6 +13,8 @@ function Swap() {
   const [tokenTwoAmount, setTokenTwoAmount] = useState(null);
   const [tokenOne, setTokenOne] = useState(tokenList[0]);
   const [tokenTwo, setTokenTwo] = useState(tokenList[1]);
+  const [isOpen, setIsOpen] = useState(false);
+  const [changeToken, setChangeToken] = useState(1);
 
   function handleSlippage(e) {
     setSlippage(e.target.value);
@@ -28,6 +30,21 @@ function Swap() {
     setTokenTwo(temp);
   }
 
+  function openModal(asset) {
+    setChangeToken(asset);
+    setIsOpen(true);
+  }
+
+  function modifyToken(i) {
+    if (changeToken === 1) {
+      setTokenOne(tokenList[i]);
+    } else {
+      setTokenTwo(tokenList[i]);
+    }
+
+    setIsOpen(false);
+  }
+
   const settings = (
     <>
       <div>Slippage Tolerance</div>
@@ -41,36 +58,66 @@ function Swap() {
     </>
   );
   return (
-    <div className="tradeBox">
-      <div className="tradeBoxHeader">
-        <h4>Swap</h4>
-        <Popover
-          content={settings}
-          title="Settings"
-          trigger="click"
-          placement="bottomRight"
-        >
-          <SettingOutlined className="cog" />
-        </Popover>
+    <>
+      <Modal
+        open={isOpen}
+        footer={null}
+        onCancel={() => setIsOpen(false)}
+        title="Select a token"
+      >
+        <div className="modalContent">
+          {tokenList?.map((e, i) => {
+            return (
+              <div
+                className="tokenChoice"
+                key={i}
+                onClick={() => modifyToken(i)}
+              >
+                <img src={e.img} alt={e.ticker} className="tokenLogo" />
+                <div className="tokenChoiceNames">
+                  <div className="tokenName">{e.name}</div>
+                  <div className="tokenTicker">{e.ticker}</div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </Modal>
+      <div className="tradeBox">
+        <div className="tradeBoxHeader">
+          <h4>Swap</h4>
+          <Popover
+            content={settings}
+            title="Settings"
+            trigger="click"
+            placement="bottomRight"
+          >
+            <SettingOutlined className="cog" />
+          </Popover>
+        </div>
+        <div className="inputs">
+          <Input
+            placeholder="0"
+            value={tokenOneAmount}
+            onChange={changeAmount}
+          />
+          <Input placeholder="0" value={tokenTwoAmount} disabled={true} />
+          <div className="switchButton" onClick={switchTokens}>
+            <ArrowDownOutlined className="switchArrow" />
+          </div>
+          <div className="assetOne" onClick={() => openModal(1)}>
+            <img src={tokenOne.img} alt="assetOneLogo" className="assetLogo" />
+            {tokenOne.ticker}
+            <DownOutlined />
+          </div>
+          <div className="assetTwo" onClick={() => openModal(2)}>
+            <img src={tokenTwo.img} alt="assetTwoLogo" className="assetLogo" />
+            {tokenTwo.ticker}
+            <DownOutlined />
+          </div>
+        </div>
       </div>
-      <div className="inputs">
-        <Input placeholder="0" value={tokenOneAmount} onChange={changeAmount} />
-        <Input placeholder="0" value={tokenTwoAmount} disabled={true} />
-        <div className="switchButton" onClick={switchTokens}>
-          <ArrowDownOutlined className="switchArrow" />
-        </div>
-        <div className="assetOne">
-          <img src={tokenOne.img} alt="assetOneLogo" className="assetLogo" />
-          {tokenOne.ticker}
-          <DownOutlined />
-        </div>
-        <div className="assetTwo">
-          <img src={tokenTwo.img} alt="assetTwoLogo" className="assetLogo" />
-          {tokenTwo.ticker}
-          <DownOutlined />
-        </div>
-      </div>
-    </div>
+    </>
   );
 }
 
