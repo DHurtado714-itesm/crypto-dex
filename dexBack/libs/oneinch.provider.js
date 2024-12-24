2e9e2142ef63c0a468b62ae3fcfcf7d63128ed52
Daniel Hurtado <81983296+DHurtado714-itesm@users.noreply.github.com>
2024-12-24 19:45:13 +0100

    feat(OneInchProvider): implement functions for token allowance approval and swap execution

---
+++ dexBack/libs/oneinch.provider.js
@@ -0,0 +1,76 @@
+import axios from "axios";
+
+const oneInchApiBaseUrl = "https://api.1inch.dev/swap/v6.0";
+const headers = {
+  Authorization: `Bearer ${process.env.ONEINCH_API_KEY}`,
+};
+
+async function approveAllowance(params) {
+  try {
+    const response = await axios.get(
+      `${oneInchApiBaseUrl}/${params.chainId}/approve/allowance`,
+      {
+        headers,
+        params: {
+          tokenAddress: params.tokenAddress,
+          walletAddress: params.walletAddress,
+        },
+      }
+    );
+
+    console.log(response.data);
+
+    return response.data.allowance;
+  } catch (error) {
+    console.error(error);
+    return "Internal Server Error";
+  }
+}
+
+async function approveTransaction(params) {
+  try {
+    const response = await axios.get(
+      `${oneInchApiBaseUrl}/${params.chainId}/approve/transaction`,
+      {
+        headers,
+        params: {
+          tokenAddress: params.tokenAddress,
+        },
+      }
+    );
+
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    return "Internal Server Error";
+  }
+}
+
+async function executeSwap(params) {
+  try {
+    const response = await axios.get(
+      `${oneInchApiBaseUrl}/${params.chainId}/swap`,
+      {
+        headers,
+        params: {
+          fromTokenAddress: params.fromTokenAddress,
+          toTokenAddress: params.toTokenAddress,
+          amount: params.amount,
+          fromAddress: params.fromAddress,
+          slippage: params.slippage,
+        },
+      }
+    );
+
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    return "Internal Server Error";
+  }
+}
+
+export const OneInchProvider = {
+  approveAllowance,
+  approveTransaction,
+  executeSwap,
+};
